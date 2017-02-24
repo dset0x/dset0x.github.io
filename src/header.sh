808d2b36f289875f3eb7ddf2329407966cb0ed3a
Dimitrios Semitsoglou-Tsiapos <kmhzsem@gmx.com>
2017-02-24 23:31:59 +0200

    init3

--- src/header.sh
+++ src/header.sh
@@ -4,9 +4,8 @@ ttl="$1"
 dsc="$2"
 key="$3"
 cat <<EOF
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<!DOCTYPE html>
+<html lang="en" xml:lang="en">
 <head>
 <title>$ttl</title>
 <meta charset="utf-8">
