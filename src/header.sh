ed12e1cbf532d9bf36987c52605f66f69e0714b7
Dimitrios Semitsoglou-Tsiapos <kmhzsem@gmx.com>
2017-02-25 21:38:56 +0200

    init3

--- src/header.sh
+++ src/header.sh
@@ -49,14 +49,16 @@ cat <<EOF
         article .cont {position: absolute; bottom: -0.4em; right: 0; background: #fffdff; font-size: small; padding-left: 1ch; z-index: 1;}
 
         /* Menu */
-        ul                  {padding-left: 0;}
+        ul                              {padding-left: 0;}
         ul#menu                         {text-align: right; margin-right: 0;}
         ul#menu li                      {display: block;}
         ul#menu li:not(last-child)      {padding: 0.2em 0 0.2em 0;}
+        ul#menu hr                      {width: 20ch; margin-right: 0; margin-top: 0; margin-bottom: 0;}
+
         /*
         ul#menu li                   {display: inline;}
         ul#menu li:not(last-child)   {padding-right: 1ch;}
-         */
+        */
 
         /* Mobile */
         @media (max-width: 65ch) {
@@ -75,7 +77,9 @@ cat <<EOF
 <nav>
     <ul id="menu">
         <li>$home</li>
+        <hr>
         <li><a href="https://github.com/search?o=desc&q=author:dset0x+&s=created&type=Issues&utf8=✓">GitHub Issues</a></li>
+        <hr>
     </ul> 
     <hr>
 </nav>
